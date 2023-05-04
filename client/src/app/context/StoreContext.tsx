99c3c8cb48703573fb0c3839384e34b34e75e932
Diamanto Skiada <diamanto9203@gmail.com>
2023-05-04 17:05:19 +0300

    basket good things add remove items

--- client/src/app/context/StoreContext.tsx
+++ client/src/app/context/StoreContext.tsx
@@ -22,4 +22,28 @@ export function useStoreContext() {
 // store provider
 export function StoreProvider({children}: PropsWithChildren<any>) {
     const [basket, setBasket] = useState<Basket | null>(null);
+
+    function removeItem(productId: number, quantity: number) {
+        if (!basket) return;
+
+        // spread operator
+        const items = [...basket.items];
+        const itemIndex = items.findIndex(i => i.productId === productId);
+
+        if (itemIndex >= 0) {
+            items[itemIndex].quantity -= quantity;
+            if (items[itemIndex].quantity === 0) items.splice(itemIndex, 1);
+
+            setBasket(prevState => {
+                return {...prevState!, items}
+            })
+        }
+    }
+
+    return (
+        // store context of our App
+        <StoreContext.Provider value={{basket, setBasket, removeItem}}>
+            {children}
+        </StoreContext.Provider>
+    )
 }
